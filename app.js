48459aa0080ca1a7b9b0c0a2e2eb2db966b8e63b
Ayush Dwivedi <ayushd100@gmail.com>
2025-06-13 18:42:19 +0530

    Add responsiveness for the mobile area only for home page right now

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-function toggleMenu(){
-    const menu = document.querySelector('.menu-links');
-    const icon = document.querySelector('.menu-icon');
-    menu.classList.toggle('open');
-    icon.classList.toggle('open');
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".menu-icon");
+  const photo = document.querySelector(".photo");
+  photo.classList.toggle("open");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
 }
